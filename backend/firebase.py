6198bd0def24b02a85d976bd9baa0f9920ceaae8
Raphael Mailu1 <raphaelmailu1@gmail.com>
2026-07-15 14:37:04 +0100

    Fix Firebase credentials loading

--- backend/firebase.py
+++ backend/firebase.py
@@ -1,12 +1,23 @@
 import os
 import json
+import base64
 import firebase_admin
 from firebase_admin import credentials, firestore
 
 if not firebase_admin._apps:
-    cred = credentials.Certificate(
-        json.loads(os.environ["FIREBASE_CREDENTIALS"])
-    )
+    if "FIREBASE_CREDENTIALS_B64" in os.environ:
+        decoded = base64.b64decode(
+            os.environ["FIREBASE_CREDENTIALS_B64"]
+        ).decode("utf-8")
+
+        cred = credentials.Certificate(
+            json.loads(decoded)
+        )
+    else:
+        cred = credentials.Certificate(
+            "studio-28-scheduling-firebase-adminsdk-fbsvc-a73a7a21e4.json"
+        )
+
     firebase_admin.initialize_app(cred)
 
 db = firestore.client()
